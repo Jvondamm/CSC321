7bcc719189ab48f56f9f32f295b5770d197c6f71
joshuavd <joshuavd@att.net>
2022-10-09 21:34:08 -0700

    cracked passwords found - some of them at least

--- lab4/crack.py
+++ lab4/crack.py
@@ -1,36 +1,43 @@
 from nltk.corpus import words
+from multiprocessing import Pool, freeze_support
+from functools import partial
 import bcrypt
 import timeit
 
-words = words.words()
-words = [word for word in words if (len(word) >= 6 and len(word) <= 8)]
-print("Number of words: ", len(words))
-
-passwords = {}
-with open('shadow_file.txt') as f:
-    for line in f:
-        password = line.split(":")[1].strip().encode('utf-8')
-        salt = line.split(":")[1][:29].encode('utf-8')
-        if salt in passwords:
-            passwords[salt].append(password)
-        else:
-            passwords[salt] = [password]
-
-counter = 0
-start = timeit.default_timer()
-
-for word in words:
-    if counter % 60 == 0:
-        print(counter)
+def loop(passwords, start, word):
     for salt in passwords:
         hashed = bcrypt.hashpw(word.encode('utf-8'), salt)
         for password in passwords[salt]:
-            counter += 1
             if hashed == password:
                 passwords[salt].remove(password)
                 stop = timeit.default_timer()
-                print("Cracked password is: ", word, " Time to crack: ", stop-start)
-                start = timeit.default_timer()
+                print("Cracked password is: ", word, " for hash: ", hashed, " Time to crack: ", stop-start)
+
+def main():
+    strings = words.words()
+    strings = [word for word in strings if (len(word) >= 6 and len(word) <= 8)]
+    print("Number of words: ", len(strings))
+
+    passwords = {}
+    with open('shadow_file.txt') as f:
+        for line in f:
+            password = line.split(":")[1].strip().encode('utf-8')
+            salt = line.split(":")[1][:29].encode('utf-8')
+            if salt in passwords:
+                passwords[salt].append(password)
+            else:
+                passwords[salt] = [password]
+    start = timeit.default_timer()
+
+    pool = Pool()
+    pool.map(partial(loop, passwords, start), strings)
+    pool.close()
+    pool.join()
+
+
+if __name__=="__main__":
+    freeze_support()
+    main()
 
 # for word in words:
 #     if counter % 15 == 0:
